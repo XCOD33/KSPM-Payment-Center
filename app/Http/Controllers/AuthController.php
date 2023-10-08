b45280c7f587b94fd96eb6cb6cb80fab0c2f81e9
XCOD33 <denistraju155@gmail.com>
2023-10-08 14:19:23 +0700

    now implement caching

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class AuthController extends Controller
 {
@@ -24,6 +25,11 @@ class AuthController extends Controller
         }
 
         $request->session()->regenerate();
+
+        Cache::remember('user', now()->addDay(), function () {
+            return Auth::user();
+        });
+
         return redirect()->intended(route('dashboard'));
     }
 }
