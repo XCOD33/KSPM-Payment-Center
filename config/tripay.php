655a04702d51c4364d2ad75cd60b41a64d6d4bde
XCOD33 <denistraju155@gmail.com>
2023-11-22 20:09:43 +0700

    add config for tripay

---
+++ config/tripay.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'tripay_api_production' => env('TRIPAY_API_KEY'),
+    'tripay_api_key' => env('TRIPAY_API_KEY'),
+    'tripay_private_key' => env('TRIPAY_PRI_KEY'),
+    'tripay_merchant_code' => env('TRIPAY_MERCHANT_CODE'),
+];
