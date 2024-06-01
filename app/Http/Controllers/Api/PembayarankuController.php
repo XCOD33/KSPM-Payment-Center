2eed0c74feb8e013b61feb8550120c702b04acc1
XCOD33 <denistraju155@gmail.com>
2024-06-01 22:48:17 +0700

    feat: implement tripay callback
    Adds a new endpoint to handle Tripay callbacks and updates the payment flow to integrate with Tripay.

--- app/Http/Controllers/Api/PembayarankuController.php
+++ app/Http/Controllers/Api/PembayarankuController.php
@@ -13,283 +13,283 @@ use Nekoding\Tripay\Tripay;
 
 class PembayarankuController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth:sanctum')->except('tripay_callback');
-    $this->user = auth('sanctum')->user();
-    $this->tripay = new Tripay(new HttpClient(config('app.tripay_api_key')));
-  }
-
-  private function get_pembayaran()
-  {
-    $user = User::where('id', $this->user->id)->first();
-    foreach ($user->roles as $role) {
-      $role_id = $role->id;
-      $pembayarans = Pembayaran::whereHas('role_pembayarans', function ($query) use ($role_id) {
-        $query->where('role_id', $role_id);
-      })->get();
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('tripay_callback');
+        $this->user = auth('sanctum')->user();
+        $this->tripay = new Tripay(new HttpClient(config('app.tripay_api_key')));
     }
-    return $pembayarans;
-  }
 
-  private function get_pembayarans_with_pembayaran_user()
-  {
-    $user = User::where('id', $this->user->id)->first();
-    foreach ($user->roles as $role) {
-      $role_id = $role->id;
-      $pembayarans = Pembayaran::whereHas('role_pembayarans', function ($query) use ($role_id) {
-        $query->where('role_id', $role_id);
-      })->with(['pembayaran_users' => function ($query) use ($user) {
-        $query->where('user_id', $user->id);
-      }])->get();
-    }
-    return $pembayarans;
-  }
-
-  private function get_pembayaran_user($pembayaran_id = null, $except_pembayaran = false)
-  {
-    $user = User::where('id', $this->user->id)->first();
-    foreach ($user->roles as $role) {
-      $role_id = $role->id;
-      if ($pembayaran_id) {
-        if ($except_pembayaran) {
-          $pembayaran_users = PembayaranUser::whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
-            $query->where('role_id', $role_id);
-          })->where('user_id', $user->id)->where('pembayaran_id', $pembayaran_id)->first();
-        } else {
-          $pembayaran_users = PembayaranUser::with('pembayaran')->whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
-            $query->where('role_id', $role_id);
-          })->where('user_id', $user->id)->where('pembayaran_id', $pembayaran_id)->first();
+    private function get_pembayaran()
+    {
+        $user = User::where('id', $this->user->id)->first();
+        foreach ($user->roles as $role) {
+            $role_id = $role->id;
+            $pembayarans = Pembayaran::whereHas('role_pembayarans', function ($query) use ($role_id) {
+                $query->where('role_id', $role_id);
+            })->get();
         }
-      } else {
-        $pembayaran_users = PembayaranUser::with('pembayaran')->whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
-          $query->where('role_id', $role_id);
-        })->where('user_id', $user->id)->get();
-      }
+        return $pembayarans;
     }
-    return $pembayaran_users;
-  }
 
-  public function simple()
-  {
-    $myBills = 0;
-    $totalMyBills = 0;
-    $paidBills = 0;
-    $totalPaidBills = 0;
-    $pembayarans = $this->get_pembayaran();
-    foreach ($pembayarans as $pembayaran) {
-      if ($pembayaran->pembayaran_users->isEmpty()) {
-        $myBills++;
-        $totalMyBills += $pembayaran->nominal;
-      } else {
-        foreach ($pembayaran->pembayaran_users as $pembayaran_user) {
-          if ($pembayaran_user->user_id == $this->user->id) {
-            if ($pembayaran_user->status == 'PAID') {
-              $paidBills++;
-              $totalPaidBills += $pembayaran_user->total;
+    private function get_pembayarans_with_pembayaran_user()
+    {
+        $user = User::where('id', $this->user->id)->first();
+        foreach ($user->roles as $role) {
+            $role_id = $role->id;
+            $pembayarans = Pembayaran::whereHas('role_pembayarans', function ($query) use ($role_id) {
+                $query->where('role_id', $role_id);
+            })->with(['pembayaran_users' => function ($query) use ($user) {
+                $query->where('user_id', $user->id);
+            }])->get();
+        }
+        return $pembayarans;
+    }
+
+    private function get_pembayaran_user($pembayaran_id = null, $except_pembayaran = false)
+    {
+        $user = User::where('id', $this->user->id)->first();
+        foreach ($user->roles as $role) {
+            $role_id = $role->id;
+            if ($pembayaran_id) {
+                if ($except_pembayaran) {
+                    $pembayaran_users = PembayaranUser::whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
+                        $query->where('role_id', $role_id);
+                    })->where('user_id', $user->id)->where('pembayaran_id', $pembayaran_id)->first();
+                } else {
+                    $pembayaran_users = PembayaranUser::with('pembayaran')->whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
+                        $query->where('role_id', $role_id);
+                    })->where('user_id', $user->id)->where('pembayaran_id', $pembayaran_id)->first();
+                }
             } else {
-              $myBills++;
-              $totalMyBills += $pembayaran->nominal;
+                $pembayaran_users = PembayaranUser::with('pembayaran')->whereHas('pembayaran.role_pembayarans', function ($query) use ($role_id) {
+                    $query->where('role_id', $role_id);
+                })->where('user_id', $user->id)->get();
             }
-          }
         }
-      }
+        return $pembayaran_users;
     }
 
-    return response()->json([
-      'success' => true,
-      'message' => 'Data pembayaran berhasil diambil',
-      'data' => [
-        'myBills' => $myBills,
-        'totalMyBills' => "Rp " . number_format($totalMyBills, 0, ',', '.'),
-        'paidBills' => $paidBills,
-        'totalPaidBills' => "Rp " . number_format($totalPaidBills, 0, ',', '.')
-      ]
-    ]);
-  }
+    public function simple()
+    {
+        $myBills = 0;
+        $totalMyBills = 0;
+        $paidBills = 0;
+        $totalPaidBills = 0;
+        $pembayarans = $this->get_pembayaran();
+        foreach ($pembayarans as $pembayaran) {
+            if ($pembayaran->pembayaran_users->isEmpty()) {
+                $myBills++;
+                $totalMyBills += $pembayaran->nominal;
+            } else {
+                foreach ($pembayaran->pembayaran_users as $pembayaran_user) {
+                    if ($pembayaran_user->user_id == $this->user->id) {
+                        if ($pembayaran_user->status == 'PAID') {
+                            $paidBills++;
+                            $totalPaidBills += $pembayaran_user->subtotal;
+                        } else {
+                            $myBills++;
+                            $totalMyBills += $pembayaran->nominal;
+                        }
+                    }
+                }
+            }
+        }
 
-  public function bills()
-  {
-    foreach ($this->get_pembayarans_with_pembayaran_user() as $pembayaran) {
-      $pembayaran_user = $pembayaran->pembayaran_users;
-      if ($pembayaran_user->isEmpty()) {
-        $pembayaran->pembayaran_users()->create([
-          'pembayaran_id' => $pembayaran->id,
-          'user_id' => $this->user->id,
-          'payment_method' => null,
-          'total_fee' => 0,
-          'subtotal' => 0,
-          'total' => 0,
-          'status' => 'UNPAID'
-        ]);
-      }
-    }
-
-    return response()->json([
-      'success' => true,
-      'message' => 'Data pembayaran berhasil diambil',
-      'data' => $this->get_pembayaran_user()
-    ]);
-  }
-
-  public function bill_detail($url)
-  {
-
-    $pembayaran = Pembayaran::where('url', $url)->first();
-    if (!$pembayaran) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Data pembayaran tidak ditemukan'
-      ], 404);
-    }
-
-    $fees = $this->tripay->getChannelPembayaran()['data'];
-    $channels = array_map(function ($item) use ($pembayaran) {
-      $item['total'] = ($item['total_fee']['flat'] + $pembayaran->nominal) + ($item['total_fee']['percent'] / 100 * ($pembayaran->nominal + $item['total_fee']['flat']));
-      return $item;
-    }, $fees);
-
-    return response()->json([
-      'success' => true,
-      'message' => 'Data pembayaran berhasil diambil',
-      'data' => [
-        'pembayaran' => $pembayaran,
-        'pembayaran_user' => $this->get_pembayaran_user($pembayaran->id, true),
-        'channels' => $channels
-      ]
-    ]);
-  }
-
-  public function pay(Request $request, $url)
-  {
-    $request->validate([
-      'payment_code' => 'required',
-    ]);
-
-    $pembayaran = Pembayaran::where('url', $url)->first();
-    if (!$pembayaran) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Data pembayaran tidak ditemukan'
-      ], 404);
-    }
-
-    $pembayaran_user = $this->get_pembayaran_user($pembayaran->id);
-
-    $dataToTripay = [
-      'method' => $request->payment_code,
-      'merchant_ref' => $pembayaran_user->invoice_id,
-      'amount' => $pembayaran->nominal,
-      'customer_name' => $this->user->name,
-      'customer_email' => $this->user->email,
-      'customer_phone' => $this->user->phone,
-      'order_items' => [
-        [
-          'name' => $pembayaran->name,
-          'price' => $pembayaran->nominal,
-          'quantity' => 1
-        ]
-      ],
-      'return_url' => 'https://localhost:8000',
-      'expired_time' => (time() + (24 * 60 * 60)), // 24 jam
-      'signature' => Signature::generate($pembayaran_user->invoice_id . $pembayaran->nominal)
-    ];
-
-    $res = $this->tripay->createTransaction($dataToTripay, Tripay::CLOSE_TRANSACTION);
-
-    if ($res->getResponse()['success'] === false) {
-      return response()->json([
-        'success' => false,
-        'message' => $res->getResponse()['message']
-      ]);
-    }
-
-    $data_response = $res->getResponse()['data'];
-    $pembayaran_user->update([
-      'payment_method' => $data_response['payment_name'],
-      'payment_method_code' => $data_response['payment_method'],
-    ]);
-
-    return $res->getResponse();
-  }
-
-  public function tripay_callback(Request $request)
-  {
-    $callbackSignature = $request->server('HTTP_X_CALLBACK_SIGNATURE');
-    $json = $request->getContent();
-    $signature = hash_hmac('sha256', $json, config('app.tripay_private_key'));
-
-    if ($signature !== (string) $callbackSignature) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Signature tidak valid'
-      ]);
-    }
-
-    if ('payment_status' !== (string) $request->server('HTTP_X_CALLBACK_EVENT')) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Event tidak valid'
-      ]);
-    }
-
-    $data = json_decode($json);
-
-    if (JSON_ERROR_NONE !== json_last_error()) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Data tidak valid'
-      ]);
-    }
-
-    $invoice_id = $data->merchant_ref;
-    $status = strtoupper((string) $data->status);
-
-    if ($data->is_closed_payment === 1) {
-      $pembayaran_user = PembayaranUser::where('invoice_id', $invoice_id)
-        ->where('status', 'UNPAID')
-        ->first();
-
-      if (!$pembayaran_user) {
         return response()->json([
-          'success' => false,
-          'message' => 'Data pembayaran tidak ditemukan atau sudah dibayar: ' . $invoice_id
+            'success' => true,
+            'message' => 'Data pembayaran berhasil diambil',
+            'data' => [
+                'myBills' => $myBills,
+                'totalMyBills' => "Rp " . number_format($totalMyBills, 0, ',', '.'),
+                'paidBills' => $paidBills,
+                'totalPaidBills' => "Rp " . number_format($totalPaidBills, 0, ',', '.')
+            ]
         ]);
-      }
-
-      switch ($status) {
-        case 'PAID':
-          $pembayaran_user->update([
-            'status' => 'PAID',
-            'total_fee' => $data->total_fee,
-            'subtotal' => $data->amount_received,
-            'total' => $data->total_amount
-          ]);
-          break;
-
-        case 'EXPIRED':
-          $pembayaran_user->update([
-            'status' => 'EXPIRED'
-          ]);
-          break;
-
-        case 'FAILED':
-          $pembayaran_user->update([
-            'status' => 'FAILED'
-          ]);
-          break;
-
-        default:
-          return response()->json([
-            'success' => false,
-            'message' => 'Status tidak valid'
-          ]);
-      }
-
-      return response()->json([
-        'success' => true,
-        'message' => 'Pembayaran berhasil'
-      ]);
     }
-  }
+
+    public function bills()
+    {
+        foreach ($this->get_pembayarans_with_pembayaran_user() as $pembayaran) {
+            $pembayaran_user = $pembayaran->pembayaran_users;
+            if ($pembayaran_user->isEmpty()) {
+                $pembayaran->pembayaran_users()->create([
+                    'pembayaran_id' => $pembayaran->id,
+                    'user_id' => $this->user->id,
+                    'payment_method' => null,
+                    'total_fee' => 0,
+                    'subtotal' => 0,
+                    'total' => 0,
+                    'status' => 'UNPAID'
+                ]);
+            }
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pembayaran berhasil diambil',
+            'data' => $this->get_pembayaran_user()
+        ]);
+    }
+
+    public function bill_detail($url)
+    {
+
+        $pembayaran = Pembayaran::where('url', $url)->first();
+        if (!$pembayaran) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data pembayaran tidak ditemukan'
+            ], 404);
+        }
+
+        $fees = $this->tripay->getChannelPembayaran()['data'];
+        $channels = array_map(function ($item) use ($pembayaran) {
+            $item['total'] = ($item['total_fee']['flat'] + $pembayaran->nominal) + ($item['total_fee']['percent'] / 100 * ($pembayaran->nominal + $item['total_fee']['flat']));
+            return $item;
+        }, $fees);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pembayaran berhasil diambil',
+            'data' => [
+                'pembayaran' => $pembayaran,
+                'pembayaran_user' => $this->get_pembayaran_user($pembayaran->id, true),
+                'channels' => $channels
+            ]
+        ]);
+    }
+
+    public function pay(Request $request, $url)
+    {
+        $request->validate([
+            'payment_code' => 'required',
+        ]);
+
+        $pembayaran = Pembayaran::where('url', $url)->first();
+        if (!$pembayaran) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data pembayaran tidak ditemukan'
+            ], 404);
+        }
+
+        $pembayaran_user = $this->get_pembayaran_user($pembayaran->id);
+
+        $dataToTripay = [
+            'method' => $request->payment_code,
+            'merchant_ref' => $pembayaran_user->uuid,
+            'amount' => $pembayaran->nominal,
+            'customer_name' => $this->user->name,
+            'customer_email' => $this->user->email,
+            'customer_phone' => $this->user->phone,
+            'order_items' => [
+                [
+                    'name' => $pembayaran->name,
+                    'price' => $pembayaran->nominal,
+                    'quantity' => 1
+                ]
+            ],
+            'return_url' => url('/dashboard/pembayaran', $pembayaran->url),
+            'expired_time' => (time() + (24 * 60 * 60)), // 24 jam
+            'signature' => Signature::generate($pembayaran_user->uuid . $pembayaran->nominal)
+        ];
+
+        $res = $this->tripay->createTransaction($dataToTripay, Tripay::CLOSE_TRANSACTION);
+
+        if ($res->getResponse()['success'] === false) {
+            return response()->json([
+                'success' => false,
+                'message' => $res->getResponse()['message']
+            ]);
+        }
+
+        $data_response = $res->getResponse()['data'];
+        $pembayaran_user->update([
+            'payment_method' => $data_response['payment_name'],
+            'payment_method_code' => $data_response['payment_method'],
+        ]);
+
+        return $res->getResponse();
+    }
+
+    public function tripay_callback(Request $request)
+    {
+        $callbackSignature = $request->server('HTTP_X_CALLBACK_SIGNATURE');
+        $json = $request->getContent();
+        $signature = hash_hmac('sha256', $json, config('app.tripay_private_key'));
+
+        if ($signature !== (string) $callbackSignature) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Signature tidak valid'
+            ]);
+        }
+
+        if ('payment_status' !== (string) $request->server('HTTP_X_CALLBACK_EVENT')) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Event tidak valid'
+            ]);
+        }
+
+        $data = json_decode($json);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data tidak valid'
+            ]);
+        }
+
+        $invoice_id = $data->merchant_ref;
+        $status = strtoupper((string) $data->status);
+
+        if ($data->is_closed_payment === 1) {
+            $pembayaran_user = PembayaranUser::where('invoice_id', $invoice_id)
+                ->where('status', 'UNPAID')
+                ->first();
+
+            if (!$pembayaran_user) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data pembayaran tidak ditemukan atau sudah dibayar: ' . $invoice_id
+                ]);
+            }
+
+            switch ($status) {
+                case 'PAID':
+                    $pembayaran_user->update([
+                        'status' => 'PAID',
+                        'total_fee' => $data->total_fee,
+                        'subtotal' => $data->amount_received,
+                        'total' => $data->total_amount
+                    ]);
+                    break;
+
+                case 'EXPIRED':
+                    $pembayaran_user->update([
+                        'status' => 'EXPIRED'
+                    ]);
+                    break;
+
+                case 'FAILED':
+                    $pembayaran_user->update([
+                        'status' => 'FAILED'
+                    ]);
+                    break;
+
+                default:
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Status tidak valid'
+                    ]);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Pembayaran berhasil'
+            ]);
+        }
+    }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,144 +10,145 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth:sanctum')->except('login');
-    $this->user = auth('sanctum')->user();
-  }
-
-  public function login(Request $request)
-  {
-    $request->validate([
-      'nim' => 'required|min:1|max:255',
-      'password' => 'required|min:8|max:255'
-    ], [
-      'nim.required' => 'NIM harus diisi',
-      'nim.min' => 'NIM minimal 10 karakter',
-      'nim.max' => 'NIM maksimal 255 karakter',
-      'password.required' => 'Password harus diisi',
-      'password.min' => 'Password minimal 8 karakter',
-      'password.max' => 'Password maksimal 255 karakter'
-    ]);
-
-    $user = User::where('nim', $request->nim)->first();
-
-    if (!$user || !password_verify($request->password, $user->password)) {
-      return response()->json([
-        'message' => 'NIM atau password salah'
-      ], 401);
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('login');
+        $this->user = auth('sanctum')->user();
     }
 
-    if (auth('sanctum')->check()) {
-      $this->user->tokens()->delete();
+    public function login(Request $request)
+    {
+        $request->validate([
+            'nim' => 'required|min:1|max:255',
+            'password' => 'required|min:8|max:255'
+        ], [
+            'nim.required' => 'NIM harus diisi',
+            'nim.min' => 'NIM minimal 10 karakter',
+            'nim.max' => 'NIM maksimal 255 karakter',
+            'password.required' => 'Password harus diisi',
+            'password.min' => 'Password minimal 8 karakter',
+            'password.max' => 'Password maksimal 255 karakter'
+        ]);
+
+        $user = User::where('nim', $request->nim)->first();
+
+        if (!$user || !password_verify($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'NIM atau password salah'
+            ], 401);
+        }
+
+        if (auth('sanctum')->check()) {
+            $this->user->tokens()->delete();
+        }
+
+        $token = $user->createToken('api-android')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login berhasil',
+            'data' => [
+                'token' => $token,
+                'nama' => $user->name,
+            ]
+        ]);
     }
 
-    $token = $user->createToken('api-android')->plainTextToken;
+    public function detail()
+    {
+        $user = User::where('id', $this->user->id)
+            ->with(['roles', 'position'])
+            ->first();
 
-    return response()->json([
-      'success' => true,
-      'message' => 'Login berhasil',
-      'data' => [
-        'token' => $token,
-        'nama' => $user->name,
-      ]
-    ]);
-  }
-
-  public function detail()
-  {
-    $user = User::where('id', $this->user->id)
-      ->with(['roles', 'position'])
-      ->first();
-
-    return response()->json([
-      'success' => true,
-      'message' => 'Detail User : ' . $user->name,
-      'data' => $user,
-    ]);
-  }
-
-  public function change_password(Request $request)
-  {
-    $request->validate([
-      'old_password' => 'required|min:8|max:255',
-      'new_password' => 'required|min:8|max:255',
-    ], [
-      'old_password.required' => 'Password lama harus diisi',
-      'old_password.min' => 'Password lama minimal 8 karakter',
-      'old_password.max' => 'Password lama maksimal 255 karakter',
-      'new_password.required' => 'Password baru harus diisi',
-      'new_password.min' => 'Password baru minimal 8 karakter',
-      'new_password.max' => 'Password baru maksimal 255 karakter'
-    ]);
-
-    $user = User::where('id', $this->user->id)->first();
-
-    if (!Hash::check($request->old_password, $user->password)) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Password lama salah'
-      ], 401);
+        return response()->json([
+            'success' => true,
+            'message' => 'Detail User : ' . $user->name,
+            'data' => $user,
+        ]);
     }
 
-    if (Hash::check($request->new_password, $user->password)) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Password baru tidak boleh sama dengan password lama'
-      ], 401);
+    public function change_password(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required|min:8|max:255',
+            'new_password' => 'required|min:8|max:255',
+        ], [
+            'old_password.required' => 'Password lama harus diisi',
+            'old_password.min' => 'Password lama minimal 8 karakter',
+            'old_password.max' => 'Password lama maksimal 255 karakter',
+            'new_password.required' => 'Password baru harus diisi',
+            'new_password.min' => 'Password baru minimal 8 karakter',
+            'new_password.max' => 'Password baru maksimal 255 karakter'
+        ]);
+
+        $user = User::where('id', $this->user->id)->first();
+
+        if (!Hash::check($request->old_password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Password lama salah'
+            ], 401);
+        }
+
+        if (Hash::check($request->new_password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Password baru tidak boleh sama dengan password lama'
+            ], 401);
+        }
+
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        $this->user->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Password berhasil diubah'
+        ]);
     }
 
-    $user->password = Hash::make($request->new_password);
-    $user->save();
+    public function logout(Request $request)
+    {
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
 
-    $this->user->tokens()->delete();
+        if (!$token) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token tidak ditemukan'
+            ], 401);
+        }
 
-    return response()->json([
-      'success' => true,
-      'message' => 'Password berhasil diubah'
-    ]);
-  }
+        if (!auth('sanctum')->check()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+        }
 
-  public function logout(Request $request)
-  {
-    $token = str_replace('Bearer ', '', $request->header('Authorization'));
+        $request->validate(
+            [
+                'nim' => 'required|min:1|max:255'
+            ],
+            [
+                'nim.required' => 'NIM harus diisi',
+                'nim.min' => 'NIM minimal 10 karakter',
+                'nim.max' => 'NIM maksimal 255 karakter'
+            ]
+        );
 
-    if (!$token) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Token tidak ditemukan'
-      ], 401);
+        $user = User::where('nim', $request->nim)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'NIM tidak ditemukan'
+            ], 404);
+        }
+        $user->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout berhasil'
+        ]);
     }
-
-    if (!auth('sanctum')->check()) {
-      return response()->json([
-        'success' => false,
-        'message' => 'Unauthorized'
-      ], 401);
-    }
-
-    $request->validate(
-      [
-        'nim' => 'required|min:1|max:255'
-      ],
-      [
-        'nim.required' => 'NIM harus diisi',
-        'nim.min' => 'NIM minimal 10 karakter',
-        'nim.max' => 'NIM maksimal 255 karakter'
-      ]
-    );
-
-    $user = User::where('nim', $request->nim)->first();
-    if (!$user) {
-      return response()->json([
-        'message' => 'NIM tidak ditemukan'
-      ], 404);
-    }
-    $user->tokens()->delete();
-
-    return response()->json([
-      'success' => true,
-      'message' => 'Logout berhasil'
-    ]);
-  }
 }
