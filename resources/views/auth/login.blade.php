c5878ca5dba9d6f0ba739f85504761677225071f
XCOD33 <denistraju155@gmail.com>
2024-12-30 21:49:56 +0700

    ♻️ (login.blade.php): merapikan struktur HTML dan indentasi pada form login
    Perubahan ini dilakukan untuk meningkatkan keterbacaan dan konsistensi kode. Dengan merapikan struktur HTML, pengembang lain dapat lebih mudah memahami dan memelihara kode di masa depan. Indentasi yang konsisten juga membantu dalam mengidentifikasi elemen-elemen yang terhubung dalam form login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,52 @@
 @extends('templates.auth.app')
 
 @section('content')
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <h1><b>KSPM</b> UTY</h1>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Masuk untuk memulai sesi Anda</p>
-
-                <form action="{{ route('login_post') }}" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Nomor Induk Mahasiswa" name="nim">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember" name="remember">
-                                <label for="remember">
-                                    Ingat Saya
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Masuk</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">Lupa Password</a>
-                </p>
-            </div>
-            <!-- /.card-body -->
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <h1><b>KSPM</b> UTY</h1>
         </div>
-        <!-- /.card -->
+        <div class="card-body">
+            <p class="login-box-msg">Masuk untuk memulai sesi Anda</p>
+
+            <form action="{{ route('login_post') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Nomor Induk Mahasiswa" name="nim">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember" name="remember">
+                            <label for="remember">
+                                Ingat Saya
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.card-body -->
     </div>
+    <!-- /.card -->
+</div>
 @endsection
